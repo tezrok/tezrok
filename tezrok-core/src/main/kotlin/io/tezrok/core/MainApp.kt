d35b083f292b05e67441526ab04c70c12c03a5dc
Ruslan A <ruslan.absalyamov@gmail.com>
2020-05-14 00:17:06 +0300

    Empty project

---
+++ tezrok-core/src/main/kotlin/io/tezrok/core/MainApp.kt
@@ -0,0 +1,14 @@
+package io.tezrok.core
+
+/**
+ * Entry point
+ *
+ * @author Ruslan Absalyamov
+ * @since 1.0
+ */
+object MainApp {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello from tezrok!")
+    }
+}
