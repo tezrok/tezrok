dbda3301384e923cc86956a6c7354eb43842a4e1
Ruslan A <ruslan.absalyamov@gmail.com>
2023-01-08 21:56:49 +0100

    Improve java test

--- tezrok-core/src/test/kotlin/com/tezrok/core/tree/NodePropertiesJavaTest.java
+++ tezrok-core/src/test/kotlin/com/tezrok/core/tree/NodePropertiesJavaTest.java
@@ -7,8 +7,13 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * Checks if works as expected when in Java
+ */
 class NodePropertiesJavaTest extends BaseTest {
 
     @Test
@@ -17,12 +22,20 @@ class NodePropertiesJavaTest extends BaseTest {
         var root = manager.getRootNode();
         var properties = root.getProperties();
         var prop = PropertyName.of("prop", "");
-        List<String> expected = List.of("value", "value2");
-        properties.setProperty(prop, expected);
-        List<String> actual = properties.getListProperty(prop);
-        Assertions.assertEquals(expected, actual);
 
+        List<String> oldList1 = properties.getListProperty(prop);
+        Assertions.assertEquals(emptyList(), oldList1);
         // try to modify the list must throw an exception
-        assertThrows(UnsupportedOperationException.class, () -> actual.add("value3"));
+        assertThrows(UnsupportedOperationException.class, () -> oldList1.add("foo"));
+
+        List<String> expected = List.of("value", "value2");
+        List<String> oldList2 = properties.setProperty(prop, expected);
+        assertNull(oldList2);
+
+        List<String> actual = properties.getListProperty(prop);
+
+        Assertions.assertEquals(expected, actual);
+        // try to modify the list must throw an exception
+        assertThrows(UnsupportedOperationException.class, () -> actual.add("bar"));
     }
 }
