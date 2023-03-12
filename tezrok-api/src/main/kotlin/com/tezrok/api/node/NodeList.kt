73c052c3ef1c44741d55277ed66d478133c1057c
Ruslan A <ruslan.absalyamov@gmail.com>
2023-03-12 20:32:17 +0100

    WIP

--- tezrok-api/src/main/kotlin/com/tezrok/api/node/NodeList.kt
+++ tezrok-api/src/main/kotlin/com/tezrok/api/node/NodeList.kt
@@ -4,9 +4,20 @@ package com.tezrok.api.node
  * Interface interpreted as a list of nodes
  */
 interface NodeList<out T> : List<T> where T : Node {
+    /**
+     * Create a new node
+     */
     fun create(name: String): T
 
+    /**
+     * Add a node
+     */
     fun add(node: @UnsafeVariance T)
 
-    fun remove(node: List<@UnsafeVariance T>): Boolean
+    /**
+     * Remove specified nodes
+     *
+     * @return true if at least one node was removed
+     */
+    fun remove(nodes: List<@UnsafeVariance T>): Boolean
 }
--- tezrok-api/src/main/kotlin/com/tezrok/api/node/ProjectNode.kt
+++ tezrok-api/src/main/kotlin/com/tezrok/api/node/ProjectNode.kt
@@ -1,7 +1,16 @@
 package com.tezrok.api.node
 
+/**
+ * Interface representing a project node
+ */
 interface ProjectNode : Node {
+    /**
+     * Get all modules of this project
+     */
     fun getModules(): ModuleNodes
 
+    /**
+     * Global settings of this project
+     */
     fun getSettings(): ProjectSettingsNode
 }
