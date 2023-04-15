95c04373e84cd1cc8989d62a13c4cbe6fa1b6a71
Ruslan A <ruslan.absalyamov@gmail.com>
2023-04-15 15:55:55 +0200

    Refactor XmlNode due to XPath

--- tezrok-cli/src/main/kotlin/io/tezrok/api/xml/XmlNode.kt
+++ tezrok-cli/src/main/kotlin/io/tezrok/api/xml/XmlNode.kt
@@ -1,37 +1,41 @@
 package io.tezrok.api.xml
 
 import io.tezrok.util.ByteArrayUtil
+import net.jcip.annotations.NotThreadSafe
 import org.apache.commons.lang3.Validate
 import org.w3c.dom.Element
 import java.io.OutputStream
 import java.util.*
-import javax.xml.parsers.DocumentBuilder
-import javax.xml.parsers.DocumentBuilderFactory
+import java.util.stream.Stream
+import java.util.stream.StreamSupport
 import javax.xml.transform.OutputKeys
 import javax.xml.transform.Transformer
 import javax.xml.transform.TransformerFactory
 import javax.xml.transform.dom.DOMSource
 import javax.xml.transform.stream.StreamResult
 
-open class XmlNode(private var name: String, private var value: String? = null, private val parent: XmlNode? = null) {
+/**
+ * Class works with xml
+ */
+@NotThreadSafe
+open class XmlNode private constructor(private val element: Element, private val parent: XmlNode? = null) {
     private val attrs: MutableMap<String, String> = HashMap()
-    private val items: MutableList<XmlNode> = ArrayList()
 
     init {
         Validate.notBlank(name, "Xml node name cannot be blank")
     }
 
-    fun getName(): String = name
+    fun getName(): String = element.tagName
 
-    fun getValue(): String? = value
+    fun getValue(): String? = element.nodeValue
 
     fun setName(name: String): XmlNode {
-        this.name = name
+        element.ownerDocument.renameNode(element, element.namespaceURI, name)
         return this
     }
 
     fun setValue(value: String?): XmlNode {
-        this.value = value
+        element.nodeValue = value
         return this
     }
 
@@ -44,71 +48,62 @@ open class XmlNode(private var name: String, private var value: String? = null, 
     /**
      * Creates a new node or returns first existing one
      */
-    @Synchronized
-    fun getOrCreate(name: String): XmlNode = items.find { p: XmlNode -> p.getName() == name } ?: add(name)
+    fun getOrCreate(name: String): XmlNode =
+        itemStream().filter { p: XmlNode -> p.getName() == name }
+            .findFirst()
+            .orElseGet { add(name) }
 
-    @Synchronized
     fun add(name: String): XmlNode {
-        val child = XmlNode(name, parent = this)
-        items.add(child)
-        return child
+        val childElem = element.ownerDocument.createElement(name)
+        return XmlNode(childElem, parent = this)
     }
 
-    @Synchronized
-    fun get(name: String): List<XmlNode> = items.filter { it.getName() == name }
+    fun get(name: String): List<XmlNode> = itemStream().filter { p: XmlNode -> p.getName() == name }.toList()
 
-    @Synchronized
     fun addAttr(name: String, value: String): XmlNode {
         attrs[name] = value
         return this
     }
 
-    @Synchronized
     fun getAttr(name: String): Optional<XmlAttr> {
         return Optional.ofNullable(attrs[name]).map { XmlAttr(name, it) }
     }
 
-    @Synchronized
     fun hasAttr(name: String): Boolean = attrs.containsKey(name)
 
-    @Synchronized
     fun removeAttr(name: String): Boolean = attrs.remove(name) != null
 
-    val isEmpty: Boolean = items.isEmpty()
+    val isEmpty: Boolean = element.childNodes.length == 0
 
-    @Synchronized
     fun getAttrs(): List<XmlAttr> {
         return attrs.map { XmlAttr(it.key, it.value) }
     }
 
-    @Synchronized
-    fun getItems(): List<XmlNode> {
-        return items.toList()
+    fun getItems(): List<XmlNode> = itemStream().toList()
+
+    protected fun itemStream(): Stream<XmlNode> {
+        val nodes = element.childNodes
+        val iterator = object : Iterator<XmlNode> {
+            private var index = 0
+            override fun hasNext(): Boolean = index < nodes.length
+            override fun next(): XmlNode {
+                val node = nodes.item(index)
+                index++
+                return XmlNode(node as Element, parent = this@XmlNode)
+            }
+        }
+
+        return StreamSupport.stream(Iterable { iterator }.spliterator(), false)
     }
 
-    @Synchronized
     fun remove(node: XmlNode): Boolean = items.remove(node)
 
     fun writeAsString(stream: OutputStream) {
-        val docBuilder: DocumentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-        val doc = docBuilder.newDocument()
-        val element = doc.createElement(name)
-        visit(element)
         val transformer: Transformer = TransformerFactory.newInstance().newTransformer()
         // pretty print
         transformer.setOutputProperty(OutputKeys.INDENT, "yes")
         transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2")
-        transformer.transform(DOMSource(doc), StreamResult(stream))
-    }
-
-    private fun visit(element: Element) {
-        val ownerDocument = element.ownerDocument
-        attrs.forEach { (key, value) -> element.setAttribute(key, value) }
-        items.forEach { node: XmlNode ->
-            val childElement = ownerDocument.createElement(node.getName())
-            element.appendChild(childElement)
-            node.visit(childElement)
-        }
+        transformer.transform(DOMSource(element.ownerDocument), StreamResult(stream))
     }
 
     override fun toString(): String = ByteArrayUtil.outputAsArray(this::writeAsString).toString(Charsets.UTF_8)
