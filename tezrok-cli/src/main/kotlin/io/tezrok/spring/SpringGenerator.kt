c22b87eb2d286f802ac345175a640b4a21499f48
Ruslan A <ruslan.absalyamov@gmail.com>
2023-05-27 20:16:16 +0200

    Spring application properties

--- tezrok-cli/src/main/kotlin/io/tezrok/spring/SpringGenerator.kt
+++ tezrok-cli/src/main/kotlin/io/tezrok/spring/SpringGenerator.kt
@@ -2,7 +2,9 @@ package io.tezrok.spring
 
 import io.tezrok.api.GeneratorContext
 import io.tezrok.api.TezrokFeature
+import io.tezrok.api.java.JavaClassNode
 import io.tezrok.api.maven.MavenDependency
+import io.tezrok.api.maven.ModuleNode
 import io.tezrok.api.maven.ProjectNode
 import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
@@ -18,25 +20,35 @@ internal class SpringGenerator : TezrokFeature {
         val pom = module.pom
         // TODO: get spring version from context
         pom.getParentNode().dependencyId = MavenDependency.of("org.springframework.boot:spring-boot-starter-parent:3.1.0")
-        pom.addPluginDependency("org.springframework.boot:spring-boot-maven-plugin")
         pom.addDependency("org.springframework.boot:spring-boot-starter")
+        pom.addPluginDependency("org.springframework.boot:spring-boot-maven-plugin")
 
         val mainClass = module.source.main.java.applicationClass
         if (mainClass != null) {
-            mainClass.addAnnotation(SpringBootApplication::class.java)
-            mainClass.addImport(SpringApplication::class.java)
-            val mainMethod = mainClass.getOrCreateMethod("main")
-            mainMethod.setJavadocComment("Entry point of the application.")
-            // TODO: add spring feature: not to clear body
-            mainMethod.clearBody()
-            mainMethod.addCallExpression("SpringApplication.run")
-                    .addNameArgument(mainClass.getName() + ".class")
-                    .addNameArgument("args")
+            handleMainMethod(mainClass)
+            addApplicationProperties(module)
         } else {
-            log.warn("Application class not found")
+            log.warn("Main application class not found")
         }
 
         return true
+    }
+
+    private fun addApplicationProperties(module: ModuleNode) {
+        val appProps = module.source.main.resources.getOrAddFile("application.properties")
+        appProps.setString("spring.main.banner-mode=off\n")
+    }
+
+    private fun handleMainMethod(mainClass: JavaClassNode) {
+        mainClass.addAnnotation(SpringBootApplication::class.java)
+        mainClass.addImport(SpringApplication::class.java)
+        val mainMethod = mainClass.getOrCreateMethod("main")
+        mainMethod.setJavadocComment("Entry point of the application.")
+        // TODO: add spring feature: not to clear body
+        mainMethod.clearBody()
+        mainMethod.addCallExpression("SpringApplication.run")
+                .addNameArgument(mainClass.getName() + ".class")
+                .addNameArgument("args")
     }
 
     private companion object {
