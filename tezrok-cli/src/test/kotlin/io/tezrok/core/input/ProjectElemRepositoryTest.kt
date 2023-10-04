1110533469cac1f246bd99d4bff48994b09b762d
Ruslan A <ruslan.absalyamov@gmail.com>
2023-10-04 21:53:52 +0200

    Fix ProjectElemRepositoryTest

--- tezrok-cli/src/test/kotlin/io/tezrok/core/input/ProjectElemRepositoryTest.kt
+++ tezrok-cli/src/test/kotlin/io/tezrok/core/input/ProjectElemRepositoryTest.kt
@@ -1,6 +1,7 @@
 package io.tezrok.core.input
 
 import io.tezrok.BaseTest
+import io.tezrok.core.feature.FeatureManager
 import io.tezrok.util.ResourceUtil
 import io.tezrok.util.resourceAsString
 import io.tezrok.util.toPrettyJson
@@ -8,11 +9,12 @@ import org.junit.jupiter.api.Test
 
 internal class ProjectElemRepositoryTest : BaseTest() {
     private val loader = ProjectElemRepository()
+    private val featureManager = FeatureManager()
 
     @Test
     fun testLoad_FromJsonWithSchema() {
         val projectPath = ResourceUtil.getResourceAsPath("/projects/tezrok-simple.json")
-        val project = loader.load(projectPath)
+        val project = loader.load(projectPath, featureManager::processModel)
 
         val actual = project.toPrettyJson()
         val expected = "/projects/tezrok-simple.expected.json".resourceAsString()
